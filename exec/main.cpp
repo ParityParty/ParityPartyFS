c3f68844c0e36bb61ca56f5c39064225becf08ba
Igor Januszkiewicz <igorjanuszkiewicz997@gmail.com>
2025-08-25 17:25:47 +0200

    Create initial project structure

---
+++ exec/main.cpp
@@ -0,0 +1,17 @@
+#include "fusepp/Fuse-impl.hpp"
+#include "ppfs/ppfs.hpp"
+#include <iostream>
+
+int main(int argc, char* argv[])
+{
+    try {
+        PpFS fs;
+        return fs.run(argc, argv); // enters FUSE loop
+    } catch (const std::exception& e) {
+        std::cerr << "Filesystem crashed 😭: " << e.what() << std::endl;
+        return 1;
+    } catch (...) {
+        std::cerr << "Filesystem crashed for unknown reasons 💀" << std::endl;
+        return 2;
+    }
+}
