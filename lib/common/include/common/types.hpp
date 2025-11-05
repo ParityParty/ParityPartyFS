c711fc1b351b39efc8159616674f58064f019e93
Igor Januszkiewicz <igorjanuszkiewicz997@gmail.com>
2025-11-05 14:33:14 +0100

    wip

---
+++ lib/common/include/common/types.hpp
@@ -0,0 +1,4 @@
+#pragma once
+
+typedef int num_entries_t;
+typedef int block_index_t;
---
+++ lib/inode_manager/include/inode_manager/inode.hpp
@@ -0,0 +1,20 @@
+#pragma once
+#include "ppfs/types.hpp"
+
+#include <array>
+
+enum FileType {
+    File,
+    Directory,
+};
+
+struct Inode {
+    FileType type;
+    unsigned int file_size;
+    unsigned long int time_creation;
+    unsigned long int time_modified;
+    std::array<block_index_t, 12> direct_blocks;
+    block_index_t indirect_block;
+    block_index_t doubly_indirect_block;
+    block_index_t trebly_indirect_block;
+};
