f164a5222c7b840ea3c9b7fa36fdc41dd3e92c63
Igor Januszkiewicz <igorjanuszkiewicz997@gmail.com>
2025-11-06 13:26:00 +0100

    Add IFilesystem methods

--- lib/filesystem/include/filesystem/ifilesystem.hpp
+++ lib/filesystem/include/filesystem/ifilesystem.hpp
@@ -1,8 +1,17 @@
 #pragma once
 #include "disk/idisk.hpp"
 
+#include <common/types.hpp>
 #include <expected>
 
 struct IFilesystem {
     std::expected<void, DiskError> format();
+    std::expected<inode_index_t, DiskError> open(std::string path);
+    std::expected<void, DiskError> close(inode_index_t inode);
+    std::expected<void, DiskError> remove(std::string path);
+    std::expected<std::vector<std::byte>, DiskError> read(
+        inode_index_t inode, size_t offset, size_t size);
+    std::expected<void, DiskError> write(
+        inode_index_t inode, std::vector<std::byte> buffer, size_t offset);
+    std::expected<inode_index_t, DiskError> createDirectory(std::string path);
 };
