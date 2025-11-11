658a39e57439247d584c1232307bd846d02bb7c6
Antonina Frackowiak <160714681+tosiaf@users.noreply.github.com>
2025-11-11 12:24:35 +0100

    25 rs block device (#26)
    * Added gf256
    
    * Redesigned gf256 constexpressions
    
    * Added polynomials
    
    * Added rs block device
    
    * Added encoding
    
    * Added integrality checking and read/write functions
    
    * Updated cmake files
    
    * Fixed errors and renamed files
    
    * First decoding version
    
    * Working versionctest --preset debugctest --preset debugctest --preset debugctest --preset debugctest --preset debug
    
    * Made GF256 class nicer
    
    * Made GF256 Polynomial class nicer
    
    * Made GF256 Polynomial class nicer
    
    * Added proper tests
    
    * Made correctable bytes and block length configurable
    
    * Updated tests
    
    * Formatted and added some minor fixes

---
+++ lib/ecc_helpers/include/ecc_helpers/gf256.hpp
@@ -0,0 +1,42 @@
+#pragma once
+
+#include <cstdint>
+#include <cstddef>
+/**
+ * Represents an element of the finite field GF(256), used in Reed-Solomon error correction.
+ * 
+ * This class encapsulates arithmetic in GF(2^8) with a fixed primitive polynomial.
+ * It supports addition, subtraction, multiplication, division, inversion, and logarithms
+ * of field elements.
+ */
+class GF256 {
+public:
+    static constexpr uint16_t PRIMITIVE_POLY = 0x11D;
+
+    GF256();
+    GF256(uint8_t v);
+    GF256(std::byte b);
+
+    explicit operator std::byte() const;
+    explicit operator uint8_t() const;
+
+    GF256 operator+(GF256 other) const;
+    GF256 operator-(GF256 other) const;
+
+    GF256 operator*(GF256 other) const;
+    GF256 operator/(GF256 other) const;
+
+    GF256 operator-() const;
+
+    bool operator==(const GF256 other) const;
+    bool operator!=(const GF256 other) const;
+
+    uint8_t log() const;
+
+    GF256 inv() const;
+
+    static GF256 getPrimitiveElement();
+
+private:
+    uint8_t _value;
+};
