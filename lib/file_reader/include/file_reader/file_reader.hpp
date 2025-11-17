2917cef61136978a82c5fdf5f152f0378fb7c619
Antonina Frąckowiak <tosiamf@gmail.com>
2025-11-17 18:49:15 +0100

    wip

--- lib/file_reader/include/file_reader/file_reader.hpp
+++ lib/file_reader/include/file_reader/file_reader.hpp
@@ -1,7 +1,18 @@
 #include "blockdevice/iblock_device.hpp"
-#include "inode_manager/iinode_manager.hpp"
+#include "inode_manager/inode.hpp"
 
 class FileReader {
-    IInodeManager _iinode_manager;
-    IBlockDevice _block_device;
+    IBlockDevice& _block_device;
+
+public:
+    /**
+     * Reads file with given inode
+     */
+    std::expected<std::vector<std::byte>, DiskError> ReadFile(Inode inode, size_t offset);
+
+    /**
+     * Writes file with given inode. Does not resize file.
+     */
+    std::expected<void, DiskError> WriteFile(
+        Inode inode, size_t offset, std::vector<std::byte> bytes_to_write);
 };
